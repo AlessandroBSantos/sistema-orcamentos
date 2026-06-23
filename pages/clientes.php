000cabbcb93229ccf3aac38caf7ee3c73fbfe95e
AlessandroBSantos <153738292+AlessandroBSantos@users.noreply.github.com>
2026-06-23 22:14:35 +0000

    atulizacao clientes

--- pages/clientes.php
+++ pages/clientes.php
@@ -6,4 +6,22 @@ require '../config/conexao.php';
 <h2>Cadastro de Clientes</h2>
 
 <form method="POST" action="salvar_cliente.php">
-    
+    Nome:
+    <input type="text" name="nome" required>
+
+    CPF/CNPJ:
+    <input type="texte" name="telefone">
+
+    Telefone:
+    <input type="text" name="telefone">
+
+    Whatsapp:
+    <input type="text" name="whatsapp">
+
+    Email:
+    <input type="text" name="email">
+
+    <button type="submit">
+        salvar
+    </button>
+</form>
