d20511aa413d894a31fb6c4ef2f587280f6a796f
AlessandroBSantos <153738292+AlessandroBSantos@users.noreply.github.com>
2026-06-23 15:54:38 -0300

    teste banco

--- config/conexao.php
+++ config/conexao.php
@@ -1,21 +1,25 @@
 <?php
+
 $host = "localhost";
 $banco = "ales7542_orcamentos";
 $usuario = "ales7542_orcamentos_user";
 $senha = "#Anjin2305!";
 
 try {
-    $pdo = new PDO("$host;dbname=$banco";charset=utf8mb4",
-        $usuario,
-        $senha,
-        );
 
-        $pdo->setAttribute(
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$banco;charset=utf8mb4",
+        $usuario,
+        $senha
+    );
+
+    $pdo->setAttribute(
         PDO::ATTR_ERRMODE,
         PDO::ERRMODE_EXCEPTION
-        );
+    );
 
 } catch (PDOException $e) {
-    echo "Erro ao conectar com o banco de dados: " . $e->getMessage();
-    exit;
+
+    die("Erro ao conectar com o banco: " . $e->getMessage());
+
 }
