9deed2d86bb8b189d706bd72097a034554f4ab1d
AlessandroBSantos <153738292+AlessandroBSantos@users.noreply.github.com>
2026-06-24 01:45:19 +0000

    atulizar dados

--- pages/atualizar_cliente.php
+++ pages/atualizar_cliente.php
@@ -1,15 +1,28 @@
 <?php
 
+session_start();
 require '../config/conexao.php';
 
 $stmt = $pdo->prepare("
-    UPDATE clientes SET
-        nome=?,
-        cpf_cnpj=?,
-        telefone=?,
-        whatsapp=?,
-        email=?
-    WHERE id=?
+INSERT INTO clientes
+(
+    nome,
+    cpf_cnpj,
+    telefone,
+    whatsapp,
+    email,
+    cep,
+    endereco,
+    numero,
+    bairro,
+    cidade,
+    estado,
+    observacoes
+)
+VALUES
+(
+    ?,?,?,?,?,?,?,?,?,?,?,?
+)
 ");
 
 $stmt->execute([
@@ -19,9 +32,15 @@ $stmt->execute([
     $_POST['telefone'],
     $_POST['whatsapp'],
     $_POST['email'],
-    $_POST['id']
+    $_POST['cep'],
+    $_POST['endereco'],
+    $_POST['numero'],
+    $_POST['bairro'],
+    $_POST['cidade'],
+    $_POST['estado'],
+    $_POST['observacoes']
 
 ]);
 
-header("Location: clientes.php?editado=1");
+header("Location: clientes.php?sucesso=1");
 exit;
--- pages/salvar_cliente.php
+++ pages/salvar_cliente.php
@@ -1,32 +1,46 @@
 <?php
 
+session_start();
 require '../config/conexao.php';
 
 $stmt = $pdo->prepare("
 INSERT INTO clientes
 (
-nome,
-cpf_cnpj,
-telefone,
-whatsapp,
-email
+    nome,
+    cpf_cnpj,
+    telefone,
+    whatsapp,
+    email,
+    cep,
+    endereco,
+    numero,
+    bairro,
+    cidade,
+    estado,
+    observacoes
 )
 VALUES
 (
-?,
-?,
-?,
-?,
-?
+    ?,?,?,?,?,?,?,?,?,?,?,?
 )
 ");
 
 $stmt->execute([
-$_POST['nome'],
-$_POST['cpf_cnpj'],
-$_POST['telefone'],
-$_POST['whatsapp'],
-$_POST['email']
+
+    $_POST['nome'],
+    $_POST['cpf_cnpj'],
+    $_POST['telefone'],
+    $_POST['whatsapp'],
+    $_POST['email'],
+    $_POST['cep'],
+    $_POST['endereco'],
+    $_POST['numero'],
+    $_POST['bairro'],
+    $_POST['cidade'],
+    $_POST['estado'],
+    $_POST['observacoes']
+
 ]);
 
 header("Location: clientes.php?sucesso=1");
+exit;
