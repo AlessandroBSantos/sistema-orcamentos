5d09a7cb230ed8b8784dcf93d38fc880841515ed
AlessandroBSantos <153738292+AlessandroBSantos@users.noreply.github.com>
2026-06-25 11:59:40 -0300

    ajuste dashboard

--- dashboard.php
+++ dashboard.php
@@ -242,7 +242,6 @@ $ultimosOrcamentos = $pdo->query("
 
 <script src="/sistema-orcamentos/assets/js/menu.js"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
