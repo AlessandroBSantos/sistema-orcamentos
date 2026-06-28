f061829c427f1c76fc1bd1e25cbd55930aae8ffc
AlessandroBSantos <153738292+AlessandroBSantos@users.noreply.github.com>
2026-06-28 23:54:58 +0000

    atualizacao menu

--- pages/menu.php
+++ pages/menu.php
@@ -1,51 +1,90 @@
-<button class="menu-toggle" onclick="toggleMenu()">
-    <i class="fa fa-bars"></i>
-        
-</button>
-
 <div class="sidebar" id="sidebar">
 
     <div class="logo">
-        <span>LLA</span> Software
+
+        <button class="menu-toggle" onclick="toggleMenu()">
+
+            <img src="/sistema-orcamentos/assets/icons/menu.svg"
+                 class="menu-icon"
+                 alt="Menu">
+
+        </button>
+
+        <div class="logo-texto">
+
+            <span>LLA</span>
+
+            <small>Software</small>
+
+        </div>
+
     </div>
 
-    <div class="menu">
+    <nav class="menu">
 
         <a href="/sistema-orcamentos/dashboard.php">
-            <i class="fa fa-chart-line"></i>
+
+            <img src="/sistema-orcamentos/assets/icons/dashboard.svg"
+                 class="menu-item-icon">
+
             Dashboard
+
         </a>
 
         <a href="/sistema-orcamentos/pages/clientes.php">
-            <i class="fa fa-users"></i>
+
+            <img src="/sistema-orcamentos/assets/icons/clientes.svg"
+                 class="menu-item-icon">
+
             Clientes
+
         </a>
 
         <a href="/sistema-orcamentos/pages/produtos.php">
-            <i class="fa fa-box"></i>
+
+            <img src="/sistema-orcamentos/assets/icons/produtos.svg"
+                 class="menu-item-icon">
+
             Produtos
+
         </a>
 
         <a href="/sistema-orcamentos/pages/orcamentos.php">
-            <i class="fa fa-file-invoice-dollar"></i>
+
+            <img src="/sistema-orcamentos/assets/icons/orcamentos.svg"
+                 class="menu-item-icon">
+
             Orçamentos
+
         </a>
 
         <a href="/sistema-orcamentos/pages/financeiro.php">
-            <i class="fa fa-chart-pie"></i>
+
+            <img src="/sistema-orcamentos/assets/icons/financeiro.svg"
+                 class="menu-item-icon">
+
             Financeiro
+
         </a>
 
         <a href="/sistema-orcamentos/pages/configuracoes.php">
-            <i class="fa fa-cog"></i>
+
+            <img src="/sistema-orcamentos/assets/icons/configuracoes.svg"
+                 class="menu-item-icon">
+
             Configurações
+
         </a>
 
         <a href="/sistema-orcamentos/logout.php">
-            <i class="fa fa-sign-out-alt"></i>
+
+            <img src="/sistema-orcamentos/assets/icons/logout.svg"
+                 class="menu-item-icon">
+
             Sair
+
         </a>
 
-    </div>
+    </nav>
 
 </div>
